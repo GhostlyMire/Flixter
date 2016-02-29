4e4c3057f5099a5427fe1675d452bbaa8c7ec9e6
vagrant <chronomaly@gmail.comgit config --global user.name GhostlyMire>
2016-02-29 19:02:08 +0000

    Fixed bug not making lessons stick

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -25,6 +25,10 @@ class Instructor::LessonsController < ApplicationController
     end
   end
 
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+
   def require_authorized_for_current_section
     if current_section.course.user != current_user
       return render text: 'Unauthorized', status: :unauthorized
