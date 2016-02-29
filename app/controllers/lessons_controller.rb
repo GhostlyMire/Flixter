49be393cee41ec2fe534452d79808284d96ae958
vagrant <chronomaly@gmail.comgit config --global user.name GhostlyMire>
2016-02-29 17:26:14 +0000

    Stripe integration

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,21 @@
 class LessonsController < ApplicationController
-  def show
-  end
+	before_action :authenticate_user!
+	before_action :require_course_enrollment_for_lessons, :only => [:show]
 
-  private
+	def show
+
+	end
+
+  	private
+
+  	def require_course_enrollment_for_lessons
+  		enrolled_course = current_lesson.section.course
+
+    	if !current_lesson.section.course.user.enrolled_in?(enrolled_course)
+    		redirect_to course_path(enrolled_course), alert: 'You must be enrolled to view lessons!'
+      	end
+	end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
